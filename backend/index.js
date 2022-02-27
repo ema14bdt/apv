0378627e1faa82292a4aa9306d2baa08903abdd1
Emanuel Romero <e.romero.1989@gmail.com>
2022-02-27 13:44:27 -0300

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import conectDB from './config/db.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js';
 import pacienteRoutes from './routes/pacienteRoutes.js';
@@ -8,6 +9,22 @@ const app = express();
 app.use(express.json()); // for parsing application/json
 dotenv.config();
 conectDB();
+
+const dominiosPermitidos = [
+    'http://localhost:3000',
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (dominiosPermitidos.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);
